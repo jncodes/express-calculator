127776c5fd6f267c2bd27d726cfcf00b82fd5082
jncodes <midn.nelson@gmail.com>
2020-02-03 20:40:22 -0500

    Add additional tests

--- calculator.test.js
+++ calculator.test.js
@@ -16,6 +16,18 @@ test('divides 9 / 3 to equal 3', () => {
     expect(ops.divide(9, 3)).toBe(3);
 });
 
-// test('adds 1 + 2 to equal 3', () => {
-//     expect(ops.add(1, 2)).toBe(3);
-// });
+test('divides 1 / 0 to equal Infinity', () => {
+    expect(ops.divide(1, 0)).toBe(Infinity);
+});
+
+test('divides 1 / Infinity to equal 0', () => {
+    expect(ops.divide(1, Infinity)).toBe(0);
+});
+
+test('divides 5 / 2 to equal 2', () => {
+    expect(ops.divide(5, 2)).toBe(2.5);
+});
+
+test('divides -1 / Infinity to equal -0', () => {
+    expect(ops.divide(-1, Infinity)).toBe(-0);
+});
